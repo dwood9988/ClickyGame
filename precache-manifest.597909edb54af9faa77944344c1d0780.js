ef57dbcd3f3d3be89a7498632c3812b87912dafe
Danielle Wood <danielle.wood9988@gmail.com>
2019-01-24 10:48:29 -0500

    Updates

---
+++ precache-manifest.597909edb54af9faa77944344c1d0780.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "fc6034d5a8af22d92c27",
+    "url": "/ClickyGame/static/js/main.fc6034d5.chunk.js"
+  },
+  {
+    "revision": "5b241c1cc36b485fa6a1",
+    "url": "/ClickyGame/static/js/1.5b241c1c.chunk.js"
+  },
+  {
+    "revision": "fc6034d5a8af22d92c27",
+    "url": "/ClickyGame/static/css/main.d6316770.chunk.css"
+  },
+  {
+    "revision": "0e988a555e94e63aa2836914ae454022",
+    "url": "/ClickyGame/index.html"
+  }
+];
